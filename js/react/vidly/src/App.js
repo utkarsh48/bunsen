b6aa4977462d71067273cae39680088c87ad789b
Utkarsh Dhiman <47657465+utkarsh48@users.noreply.github.com>
2021-04-27 18:21:55 +0000

    made it...code only

--- js/react/vidly/src/App.js
+++ js/react/vidly/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import './App.css';
 import Movies from './components/movies';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
+import './App.css';
 
 function App() {
   return (
-    <main className="container">
-			<Movies />
-		</main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies"></Redirect>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
