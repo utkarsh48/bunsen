12d1f8aee8f6f749b88c24aafcdfddca05e4c08a
Utkarsh Dhiman <utkarshdhiman48@gmail.com>
2021-07-05 15:08:10 +0530

    mod: basic correct logic

--- java/com.ut.dsa/Main.java
+++ java/com.ut.dsa/Main.java
@@ -5,23 +5,27 @@ import com.ut.dsa.arrays.Array;
 public class Main {
   public static void main(String[] args) {
     Array arr = new Array(3);
+    message("Arrays", 10);
+    message("insert", 5);
     arr.insert(10);
     arr.insert(20);
     arr.insert(30);
     arr.print();
-    seperator(5);
+    message("removeAt", 5);
     arr.removeAt(0);
+    message("insert", 5);
     arr.insert(40);
     arr.insert(50);
     arr.print();
-    seperator(10);
+    message("indexOf", 5);
+    System.out.println(arr.indexOf(-1));
   }
 
 
-  public static void seperator (int length) {
+  public static void message (String message, int length) {
     String str = "";
     for (int i = 0; i < length; i++)
       str+='-';
-    System.out.println(str);
+    System.out.println(str + message + str);
   }
 }
--- java/com.ut.dsa/arrays/Array.java
+++ java/com.ut.dsa/arrays/Array.java
@@ -2,47 +2,55 @@ package com.ut.dsa.arrays;
 
 public class Array {
   private int[] items;
-  private int index;
+  private int count;
 
   public Array(int size){
     this.items = new int[size];
-    this.index = -1;
   }
 
   public void insert(int value) {
-    if(this.index + 2 > this.items.length) {
+    if(this.count == this.items.length) {
       // new temp array
-      int[] temp = new int[this.items.length + 1];
+      int[] temp = new int[this.count * 2];
 
       // copying existing items
-      for(int i = 0; i < this.items.length; i++)
+      for(int i = 0; i < this.count; i++)
         temp[i] = this.items[i];
 
       // changing reference
       this.items = temp;
     }
     // inserting last item
-    this.items[++this.index] = value;
+    this.items[this.count++] = value;
   }
 
-  public int removeAt(int place) {
-    // temproraily storing item to remove to return later
-    int temp = this.items[place];
-    // shifting items from place till end
-    for(int i = place; i < this.index; i++)
+  public void removeAt(int index) {
+    // input validation
+    if(index < 0 || index >= this.count)
+      throw new IllegalArgumentException();
+
+    // shifting items from index till end
+    for(int i = index; i < this.count -1; i++)
       this.items[i] = this.items[i+1];
-    
+
     // reducing index
-    this.index--;
-    return temp;
+    this.count--;
+  }
+
+  public int indexOf(int item) {
+    for (int i = 0; i < this.count; i++) 
+      if(this.items[i] == item)
+        return i;
+  
+    return -1;
   }
 
   public void print() {
-    for (int i = 0; i <= this.index; i++)
+    for (int i = 0; i < this.count; i++)
       System.out.println(this.items[i]);
   }
 
   public int size() {
-    return this.index;
+    return this.count;
   }
 }
